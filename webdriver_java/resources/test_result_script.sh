787b862ffed6d22e78f134c0b5b4021a51f0b93b
Fleisher <naomif@hometalk.com>
2023-01-12 14:09:21 +0200

    spacing for github actions

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -16,6 +16,4 @@ github_actions_total_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4
 github_actions_passed_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
 github_actions_failed_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
 github_actions_skipped_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
-
-
 EOF

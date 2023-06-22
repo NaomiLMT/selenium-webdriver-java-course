ac3049182fb903f3596b6da92ec454d8fe4e3182
Fleisher <naomif@hometalk.com>
2023-06-22 09:53:07 +0300

    spacing push to kick off git actions

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -4,7 +4,7 @@
 #     This is extracting the values for each test suite description.                              #
 #     It uses awk to print the value between two quotes.                                          #
 #     The columns printed is based on the following example string:                               #
-#     <testng-results ignored="0" total="23" passed="22" failed="1" skipped="0">                  #
+#     <testng-results ignored="0" total="23" passed="22" failed="1" skipped="0">                 #
 ###################################################################################################
 
 TEST_RESULTS_LOCATION="${1:-/home/runner/work/selenium-webdriver-java-course/selenium-webdriver-java-course/webdriver_java/target/surefire-reports}"

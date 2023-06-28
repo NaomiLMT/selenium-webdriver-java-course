4e5104f0098f1c57130428d88dffe9e9727e3e66
Fleisher <naomif@hometalk.com>
2023-06-28 13:36:58 +0300

    switching to one curl request for all metrics

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -10,9 +10,10 @@ set -euo pipefail
 TEST_RESULTS_LOCATION="${1:-/home/runner/work/selenium-webdriver-java-course/selenium-webdriver-java-course/webdriver_java/target/surefire-reports}"
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 
-echo "github_actions_ignored_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $2 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-echo "github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $4 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-echo "github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $6 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-echo "github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $8 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-echo "github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $10 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-
+cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
+github_actions_ignored_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $2 }')
+github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $4 }')
+github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $6 }')
+github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $8 }')
+github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'\"' '{ print $10 }')
+EOF
